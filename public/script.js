3cd430a8d24d7eac9e1b14cb40d6b2d149ca5cd1
Hoon Lee <yl.hoony@gmail.com>
2017-05-08 01:52:54 -0700

    added vue script for add-to-cart

--- public/script.js
+++ public/script.js
@@ -1,1 +1,24 @@
-console.log('It works.');
+// console.log(Vue);
+
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      { title: 'item 1' },
+      { title: 'item 2' },
+      { title: 'item 3' }
+    ],
+    cart: [
+
+    ]
+  },
+  methods: {
+    addItem: function(index) {
+      // console.log('addItem');
+      // console.log('this:', this);
+      console.log(index);
+      this.total += 9.99;
+    }
+  }
+});
